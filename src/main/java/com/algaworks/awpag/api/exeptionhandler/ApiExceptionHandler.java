568e333c2b1efb3f11fbc10c1e377642ab027daa
amauri-castro <amauri.p.castro@gmail.com>
2024-03-22 10:23:35 -0300

    implementado metodos para capturar outras exceptions e criacao de mensagens personalizadas de erros

--- src/main/java/com/algaworks/awpag/api/exeptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/awpag/api/exeptionhandler/ApiExceptionHandler.java
@@ -1,16 +1,55 @@
 package com.algaworks.awpag.api.exeptionhandler;
 
 import com.algaworks.awpag.domain.exception.NegocioException;
-import org.springframework.http.ResponseEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.URI;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private final MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://awpag.com/errors/campos-invalidos"));
+
+        var fields = ex.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(error -> ((FieldError)error).getField(),
+                        error -> messageSource.getMessage(error, LocaleContextHolder.getLocale())));
+        problemDetail.setProperty("fields", fields);
+
+        return super.handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<String> capturar(NegocioException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ProblemDetail handleNegocio(NegocioException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle(e.getMessage());
+        problemDetail.setType(URI.create("https://awpag.com/errors/regra-de-negocio"));
+        return problemDetail;
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ProblemDetail handleDataIntegrityViolation(DataIntegrityViolationException e){
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+        problemDetail.setTitle("Recurso está em uso");
+        problemDetail.setType(URI.create("https://awpag.com/errors/recurso-em-uso"));
+        return problemDetail;
     }
 }
